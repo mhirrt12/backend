9d7b884e71190b3a48471d59bf2a29a23de1aff7
mhirrt12 <kaletamene90@gmail.com>
2026-05-18 07:29:16 -0700

    Add .htrouter.php for request handling and refactor index.php to improve routing logic

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-// .htrouter.php
-$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
-    return false;
-}
-require_once __DIR__ . '/public/index.php';
+
 
 // ==================== PHPMailer ====================
 require_once __DIR__ . '/../vendor/phpmailer/phpmailer/src/PHPMailer.php';
